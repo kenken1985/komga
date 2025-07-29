a19c1f72a2f728954df36e30f2eb728e152137a5
kenken1985 <kenken1985@gmail.com>
2025-07-29 07:11:43 -0400

    Undo multiple book push bar

--- komga/src/main/kotlin/org/gotson/komga/interfaces/api/rest/dto/PushToKindleRequestDto.kt
+++
@@ -1,9 +0,0 @@
-package org.gotson.komga.interfaces.api.rest.dto
-
-import jakarta.validation.constraints.NotEmpty
-
-data class PushToKindleRequestDto(
-  @field:NotEmpty
-  val bookIds: List<String>,
-)
-
