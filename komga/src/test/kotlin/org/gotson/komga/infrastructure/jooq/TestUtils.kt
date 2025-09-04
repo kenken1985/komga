e7b70994a9346d3fd415c99b8ccc9c81a701b916
kenken1985 <kenken1985@gmail.com>
2025-09-04 07:21:57 +0000

    Remove ignored files from repo 5

--- komga/src/test/kotlin/org/gotson/komga/infrastructure/jooq/TestUtils.kt
+++
@@ -1,7 +0,0 @@
-package org.gotson.komga.infrastructure.jooq
-
-import org.assertj.core.api.Assertions.within
-import org.assertj.core.data.TemporalUnitOffset
-import java.time.temporal.ChronoUnit
-
-val offset: TemporalUnitOffset = within(3, ChronoUnit.SECONDS)
